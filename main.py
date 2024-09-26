fbf10091e7ba2562610cce3bd7132217ca7478f8
Sankrita Patel <patelsankrita05@gmail.com>
2024-09-26 18:59:38 +0530

    Data Transformation Added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 from src.EduMetrics.logger import logging
 from src.EduMetrics.exception import CustomException
 from src.EduMetrics.components.data_ingestion import DataIngestion
+from src.EduMetrics.components.data_transformation import DataTransformation
 
 if __name__=="__main__":
     logging.info("The execution has started...")
@@ -9,6 +10,9 @@ if __name__=="__main__":
     try:
         data_ingestion = DataIngestion()
         train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
+        data_transformation = DataTransformation()
+        train_arr, test_arr,_ = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+        
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e, sys)
