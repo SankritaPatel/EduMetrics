5762bb1c107ffbf3324b0e601a77686731962516
Sankrita Patel <patelsankrita05@gmail.com>
2024-09-26 19:05:35 +0530

    Model Trainer Added

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from src.EduMetrics.logger import logging
 from src.EduMetrics.exception import CustomException
 from src.EduMetrics.components.data_ingestion import DataIngestion
 from src.EduMetrics.components.data_transformation import DataTransformation
+from src.EduMetrics.components.model_trainer import ModelTrainer
+
 
 if __name__=="__main__":
     logging.info("The execution has started...")
@@ -12,6 +14,9 @@ if __name__=="__main__":
         train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
         data_transformation = DataTransformation()
         train_arr, test_arr,_ = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+        model_trainer = ModelTrainer()
+        r2Score = model_trainer.initiate_model_trainer(train_arr, test_arr)
+        print(r2Score)
         
     except Exception as e:
         logging.info("Custom Exception")
